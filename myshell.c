46d78f638311034a6b83e25f2e5b767b8e0f7c24
jackychen071404 <jchen071404@gmail.com>
2024-09-10 00:47:18 -0400

    basic way to accept line inputs

--- myshell.c
+++ myshell.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
+#define MAX_LINE 512
 
 int main() {
-  printf("LOVE YOU!\n");
+  char line[MAX_LINE];
+  while (1) {
+    printf("my_shell$");
+    fgets(line, MAX_LINE, stdin);
+    printf("\n%s",line);
+  }
   return 0;
 }
