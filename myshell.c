e6faabb369a30f5dc33235aa0abe2fe9a4a89831
jackychen071404 <jchen071404@gmail.com>
2024-09-10 01:05:53 -0400

    exit with ctrl-d

--- myshell.c
+++ myshell.c
@@ -6,9 +6,13 @@
 
 int main() {
   char line[MAX_LINE];
+  
   while (1) {
     printf("my_shell$");
-    fgets(line, MAX_LINE, stdin);
+    if (fgets(line, MAX_LINE, stdin) == NULL) {
+      printf("\nExiting shell. Thank you!!!!...\n");
+      break;
+    }
     printf("\n%s",line);
   }
   return 0;
