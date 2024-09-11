e2d7ea17a6ad061d3ee4867ce7e94df3839882d0
jackychen071404 <jchen071404@gmail.com>
2024-09-11 00:09:08 -0400

    found problems with cd. going to make a separate handler for cd

--- myshell.h
+++ myshell.h
@@ -5,6 +5,4 @@ void line_parser(char *line, char **split_line);
 int is_valid_command(char * command);
 void execute_command(char ** command);
 void execute_cd(char ** command);
-void execute_commandd(char ** command);
-void execute_ls(char ** command);
 #endif
